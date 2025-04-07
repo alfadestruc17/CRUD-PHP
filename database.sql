312177eb744d1e2f1b46ea014dcd0aa10e756437
Tkex555 <bahamutthero@gmail.com>
2025-04-07 10:00:27 -0500

    cambios en la longitud de los datos

--- database.sql
+++ database.sql
@@ -5,12 +5,13 @@ CREATE TABLE `usuarios` (
 	`primer_apellido` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
 	`segundo_apellido` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
 	`edad` INT(10) NULL DEFAULT NULL,
-	`telefono` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
+	`telefono` VARCHAR(10) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
 	`fecha_nacimiento` DATE NULL DEFAULT NULL,
-	`correo` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-	`direccion` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
+	`correo` VARCHAR(30) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
+	`direccion` VARCHAR(15) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
+AUTO_INCREMENT=3
 ;
