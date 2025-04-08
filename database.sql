55213ee0b12fbfdf71c8ab38d6630acf80de2564
Samuel <rene28609@gmail.com>
2025-04-08 06:43:51 -0500

    arreglo base de datos

--- database.sql
+++ database.sql
@@ -1,4 +1,7 @@
-CREATE TABLE `usuarios` (
+CREATE DATABASE IF NOT EXISTS `usuarios_app` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `usuarios_app`;
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
 	`id` INT(10) NOT NULL AUTO_INCREMENT,
 	`primer_nombre` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
 	`segundo_nombre` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
